f25d6654f1ec82546157eff002c67c930431e7ac
Nissim Shitrit <nissim.shitrit2@microfocus.com>
2024-05-20 22:24:41 +0300

    user story #3001: Update RestServlet.java

--- webapp/src/main/java/com/hp/devops/demoapp/RestServlet.java
+++ webapp/src/main/java/com/hp/devops/demoapp/RestServlet.java
@@ -49,6 +49,7 @@ public class RestServlet extends HttpServlet {
 		}
 	}
 
+	
 	private void serveBands(String[] pathNodes, HttpServletResponse response) throws IOException {
 		JSONArray resBody = new JSONArray();
 		response.setContentType("application/json");
