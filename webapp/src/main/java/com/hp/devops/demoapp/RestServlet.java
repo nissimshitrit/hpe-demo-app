23ba0c8597ebd7db9d765670c9d82c5ce02d4c19
Nissim Shitrit <nissim.shitrit2@microfocus.com>
2024-05-21 08:44:37 +0300

    user story #3001: Update RestServlet.java

--- webapp/src/main/java/com/hp/devops/demoapp/RestServlet.java
+++ webapp/src/main/java/com/hp/devops/demoapp/RestServlet.java
@@ -19,8 +19,6 @@ public class RestServlet extends HttpServlet {
 		ServletContext servletContext = getServletContext();
 		DataManager.init(servletContext);
 	}	
-
-	
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
