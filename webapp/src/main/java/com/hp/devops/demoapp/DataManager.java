074bd8a1c5d1b821d85070ef20783b6cb380b3f5
Nissim Shitrit <nissim.shitrit2@microfocus.com>
2024-05-22 10:01:14 +0300

    user story #3002: Update RestServlet.java

--- webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
+++ webapp/src/main/java/com/hp/devops/demoapp/DataManager.java
@@ -32,8 +32,7 @@ public class DataManager {
 		} else {
 			throw new InvalidParameterException("servletContext   must   not be null");
 		}
-	}	
-	
+	}		
 
 	static boolean isInitialized() {
 		return initialized;
