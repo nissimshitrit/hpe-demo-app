659f8a00a0ed89615fe1f25b1aff46594edc1490
Nissim Shitrit <nissim.shitrit2@microfocus.com>
2024-06-16 11:31:32 +0300

    user story #2492034: Update RestServlet.java

--- webapp/src/main/java/com/hp/devops/demoapp/RestServlet.java
+++ webapp/src/main/java/com/hp/devops/demoapp/RestServlet.java
@@ -13,6 +13,7 @@ import java.io.IOException;
  * To change this template use File | Settings | File Templates.
  */
 public class RestServlet extends HttpServlet {
+	
 	@Override
 	public void init() throws ServletException {
 		ServletContext servletContext = getServletContext();
