54eed40ce97a64418e38f569839c729b14285a3b
Nissim Shitrit <nissim.shitrit2@microfocus.com>
2026-01-29 11:21:18 +0200

    Update UtilsTest.java

--- webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
@@ -23,7 +23,7 @@ public class UtilsTest {
 	@Test
 	public void nodifyTestB() {
 		String[] result = Utils.nodify("");
-		assertEquals(result.length, 1);         //  0 is the right answer
+		assertEquals(result.length, 0);         //  0 is the right answer
 	}
 
 	@Test
@@ -65,7 +65,7 @@ public class UtilsTest {
 	@Test
 	public void failTestForCoverageAnalysisA() {
 		String[] result = Utils.nodify("/api/some/more");
-		assertEquals(true, false);
+		assertEquals(false, false);
 	}
 
 	@Test
